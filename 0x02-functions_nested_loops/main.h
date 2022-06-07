f356d4188ea2f734ab6ce2af1da0bcc1333e83d0
habtamuararsie <habtamuararsie@yahoo.com>
2022-06-07 15:41:08 +0300

    main header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/*
+ * This one is Header File: main.h
+ * Auth: Habtamu Ararsie
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
+ */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
