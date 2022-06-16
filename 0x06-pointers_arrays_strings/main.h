6d0123af37e9f9d3c34317cc949fd7ce5f4d2cc6
habtamuararsie <habtamuararsie@yahoo.com>
2022-06-16 17:07:49 +0300

    main header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,8 @@
  * Auth:Habtamu A.
  * Desc: Header file containing prototypes for all fucntions
  *   used in the 0x06-pointers_arrays_strings directory.
- *   */
+ *  
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
