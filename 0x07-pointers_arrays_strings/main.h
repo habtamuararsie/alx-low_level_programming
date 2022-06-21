ee8ba3af1f8423ccceb3f1393c2af3f07522e29d
habtamuararsie <habtamuararsie@yahoo.com>
2022-06-21 11:29:24 +0300

    main file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
  *       used in the 0x07-pointers_arrays_strings directory.
  */
 
+#include <stddef.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -22,6 +24,15 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void set_string(char **s, char *to);
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 
 #endif
