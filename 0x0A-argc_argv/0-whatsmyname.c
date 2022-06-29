d88257ad2c08db6eb1bce965dfc523fd03b261f5
habtamuararsie <habtamuararsie@yahoo.com>
2022-06-29 15:27:28 +0300

    what is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -13,11 +13,8 @@
 
 int main(int argc, char **argv)
 {
-/*	if (argc >= 1) 
- *
- */
-     (void)argc;
-	printf("%s\n", argv[0]);
+            if (argc >= 1) 
+        printf("%s\n", argv[0]);
 
 	return (0);
 }
