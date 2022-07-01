989cb44b49f79a9fa81d5df8c1f7c9b3b2cc9709
habtamuararsie <habtamuararsie@yahoo.com>
2022-07-01 16:40:07 +0300

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *h = malloc(b);
