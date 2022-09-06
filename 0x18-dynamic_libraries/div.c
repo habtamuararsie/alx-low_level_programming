5852e0c0038178274a0cf348c3b7a32e5ee72908
habtamuararsie <habtamuararsie@yahoo.com>
2022-09-06 08:47:28 +0300

    division

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * div - Divides two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int div(int num1, int num2)
+{
+        return (num1 / num2);
+}
